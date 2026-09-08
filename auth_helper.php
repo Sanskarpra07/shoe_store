92aa9e647454824c72c8fb6735f49db127a1241f
Sanskarpra07 <pradhansanskar07@gmail.com>
2026-09-08 09:27:02 +0545

    Fix duplicated session_start() causing PHP notices on every frontend page

--- auth_helper.php
+++ auth_helper.php
@@ -1,6 +1,8 @@
 <?php
 // Common helper functions for the store front
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once __DIR__ . '/db.php';
 
 function is_customer_logged_in() {
