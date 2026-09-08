8786e801175141eda1cfcd08dbc72895c7e2a814
Sanskarpra07 <pradhansanskar07@gmail.com>
2026-09-08 17:43:51 +0545

    refactor(admin): update footer asset paths

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,6 @@
 </div>
 </div>
-<script src="../js/notify.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="../assets/js/notify.js"></script>
 </body>
 </html>
